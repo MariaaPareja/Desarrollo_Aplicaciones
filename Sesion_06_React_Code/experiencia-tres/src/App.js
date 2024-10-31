0ed913397b650e0980c36d5598b5948365c04993
MariaaPareja <maria.pareja@ucsm.edu.pe>
2024-10-31 15:55:57 -0500

    Usando import en App

--- Sesion_06_React_Code/experiencia-tres/src/App.js
+++ Sesion_06_React_Code/experiencia-tres/src/App.js
@@ -1,5 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; 
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer/Footer';
+import DataList from './DataList'; 
+import ToggleButton from './ToggleButton/ToggleButton';
+import Boton from './UseState/Boton';
+import Efecto from './UseEffect/Efecto';
 
 function App() {
   return (
