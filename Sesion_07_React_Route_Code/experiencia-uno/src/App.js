b9ff2640e725c27b5b63927321b0988ba7f6bce2
MariaaPareja <maria.pareja@ucsm.edu.pe>
2024-10-31 23:38:39 -0500

    Rutas del proyecto de tienda de ropa

--- Sesion_07_React_Route_Code/experiencia-uno/src/App.js
+++ Sesion_07_React_Route_Code/experiencia-uno/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Products from './components/Products';
+import About from './components/About';
+import ProductDetail from './components/ProductDetail';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="products" element={<Products />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="products/:id" element={<ProductDetail />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
