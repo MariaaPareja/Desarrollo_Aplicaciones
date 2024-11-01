41e175423e5495b2d963ad3d3106423d67822ae1
MariaaPareja <maria.pareja@ucsm.edu.pe>
2024-10-31 20:10:12 -0500

    Creando rutas y más componentes, usando también button

--- Sesion_06_React_Code/experiencia-cuatro/src/App.js
+++ Sesion_06_React_Code/experiencia-cuatro/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Boton from './Boton';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import './ToggleButton.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/boton">Toggle Button</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/boton" component={Boton} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ Sesion_06_React_Code/experiencia-cuatro/src/Contact.js
@@ -0,0 +1,8 @@
+// Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return <h2>Contacto: maria.pareja@ucsm.edu.pe</h2>;
+};
+
+export default Contact;
