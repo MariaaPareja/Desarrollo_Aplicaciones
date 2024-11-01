e7ec66c642bba0629e4f35f7992b35dfc745147b
MariaaPareja <maria.pareja@ucsm.edu.pe>
2024-10-31 19:45:17 -0500

    Use-effect sobre botón

--- Sesion_06_React_Code/experiencia-cuatro/src/Boton.js
+++ Sesion_06_React_Code/experiencia-cuatro/src/Boton.js
@@ -1,13 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './ToggleButton.css';
 
 function Boton() {
   const [encendido, setEncendido] = useState(false);
 
   const toggleEncendido = () => {
-    setEncendido(!encendido);
+    setEncendido(prevEncendido => !prevEncendido);
   };
 
+  // useEffect 
+  useEffect(() => {
+    if (encendido) {
+      console.log('El botón está encendido');
+    } else {
+      console.log('El botón está apagado');
+    }
+  }, [encendido]); // This runs whenever 'encendido' changes
+
+  useEffect(() => {
+    localStorage.setItem('encendido', JSON.stringify(encendido));
+  }, [encendido]); // Este efecto también se ejecuta cuando 'encendido' cambia
+
+  // Recuperar el estado de localStorage al cargar el componente
+  useEffect(() => {
+    const storedEncendido = localStorage.getItem('encendido');
+    if (storedEncendido) {
+      setEncendido(JSON.parse(storedEncendido));
+    }
+  }, []); // Este efecto solo se ejecuta una vez al montar el componente
+  
   const backgroundColor = encendido ? '#fff' : '#4586a0';
   const textColor = encendido ? '#4586a0' : '#fff';
 
@@ -22,3 +43,4 @@ function Boton() {
 }
 
 export default Boton;
+
