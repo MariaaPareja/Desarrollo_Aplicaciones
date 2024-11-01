e10ca85f5eae2f8172df9f47d00df84939219ddf
MariaaPareja <maria.pareja@ucsm.edu.pe>
2024-10-31 23:32:08 -0500

    About página tienda

---
+++ Sesion_07_React_Route_Code/experiencia-uno/src/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>Lady Stardust is dedicated to providing the best fashion for alternative people.</p>
+        </div>
+    );
+}
+
+export default About;
