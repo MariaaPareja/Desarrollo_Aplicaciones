215f4129e2de5723a40f4ec552a5887cf35fbc98
MariaaPareja <maria.pareja@ucsm.edu.pe>
2024-11-01 01:06:13 -0500

    Manejando rutas en app.js

--- Sesion_07_React_Route_Code/experiencia-uno-juegos/src/App.js
+++ Sesion_07_React_Route_Code/experiencia-uno-juegos/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                {/* Ruta principal que utiliza el Layout */}
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} /> {/* Ruta para la página de inicio */}
+                    <Route path="about" element={<About />} /> {/* Ruta para la página "Acerca de nosotros" */}
+                    <Route path="products" element={<Products />} /> {/* Ruta para mostrar la lista de productos */}
+                    {/* Ruta dinámica para mostrar detalles del producto basado en su ID */}
+                    <Route path="products/:id" element={<ProductDetail />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
