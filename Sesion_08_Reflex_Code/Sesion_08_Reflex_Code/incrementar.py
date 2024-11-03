18a3ab41ee9d3cb604b3c6809df2e8ffe4b70145
MariaaPareja <maria.pareja@ucsm.edu.pe>
2024-11-02 20:43:12 -0500

    Contador con reflex

---
+++ Sesion_08_Reflex_Code/Sesion_08_Reflex_Code/incrementar.py
@@ -0,0 +1,32 @@
+
+
+import reflex as rx
+
+from rxconfig import config
+
+class State(rx.State):
+    conteo: int= 0
+    def incrementar(self):
+        self.conteo += 1
+    def disminuir(self):
+        self.conteo -= 1
+    def on_load(self):
+        self.conteo = 0
+    def contador():
+        return rx.fragment(
+            rx.hstack(
+                rx.button("Incrementar", on_click=State.incrementar), rx.text(State.conteo),
+                rx.button("Disminuir", on_click=State.disminuir), )
+        )
+    def index() -> rx.Component:
+        return rx.container(
+            rx.color_mode.button(position="center"), 
+            rx.vstack(
+                rx.heading("Contar", size="30"), contador(),
+            ),
+            rx.logo(),
+        )
+
+app = rx.App() 
+app.add_page(index)
+
