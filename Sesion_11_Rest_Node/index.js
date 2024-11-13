9f0547260ce85dc2feef312738a6bfb9a2b23f76
MariaaPareja <maria.pareja@ucsm.edu.pe>
2024-11-12 20:53:35 -0500

    Iniciando a conectar una db y usando postman para verla

---
+++ Sesion_11_Rest_Node/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'CocaCola.020605',
+    database: 'libreria'
+})
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err);
+        return;
+    }
+    console.log('Conexión exitosa a la BD');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Creando servidor
+var server = app.listen(3000, "127.0.0.1", function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log(`Servidor escuchando en http://${host}:${port}`);
+});
+
+//API para extraer los BOOKS
+app.get('/books', function(req,res) {
+    connection.query('select * from books',function(error,results){
+        if (error) throw error;
+        res.end(JSON.stringify(results))
+    });
+});
