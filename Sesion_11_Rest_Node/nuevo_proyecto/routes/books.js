4d0d8c62828596cfabd63d1a43c7fd2d7ab3e646
MariaaPareja <maria.pareja@ucsm.edu.pe>
2024-12-08 18:51:25 -0500

    Agregando endpoint para insertar libros

--- Sesion_11_Rest_Node/nuevo_proyecto/routes/books.js
+++ Sesion_11_Rest_Node/nuevo_proyecto/routes/books.js
@@ -8,6 +8,7 @@ appRouter.use(bodyParser.urlencoded({extended:true}));
 appRouter.use(bodyParser.json());
 
 let sql_all = 'CALL usp_listar_books()';
+let sql_insert = 'CALL usp_insertar_books()';
 
 appRouter.get('/books', (req,res) => {
     con.query(sql_all,(error,results) => {
@@ -17,5 +18,21 @@ appRouter.get('/books', (req,res) => {
         res.send(results);
     })
 });
+
+appRouter.post('/books', (req,res) => {
+    const book = {
+        book_title: req.body.book_title,
+        book_author: req.body.book_author,
+        book_published: req.body.book_published
+    }
+    con.query(sql_insert, [book.book_title,book.book_author,book.book_published],
+        (error,results) => {
+            if (error) {
+                throw error;
+            }
+            res.send(results);
+        }
+    )
+})
 
 module.exports=appRouter;
